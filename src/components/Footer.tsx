b8279b55ea7add0d360dd08b8afe087731e5590a
subash <subashchandraboseravi45@gmail.com>
2025-02-17 05:27:40 +0530

    footer fixed

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@ import SocialIcons from "./SocialIcons";
 
 export default function Footer() {
     return (
-        <div className="bg-black mt-40 mb-10">
+        <div className="bg-black mt-20">
             <motion.div 
                 className="max-w-[1440px] mx-auto px-5 lg:px-20 flex flex-col items-center"
                 initial={{ opacity: 0, y: 50 }}
@@ -12,7 +12,7 @@ export default function Footer() {
                 viewport={{ once: true }}
             >
                 {/* Connect with us section */}
-                <div className="w-full text-center mb-16">
+                <div className="w-full text-center mb-8">
                     <span className="text-white font-bold text-[28px] lg:text-[27px] tracking-[4px] inline-block">
                         Connect with us
                     </span>
@@ -20,47 +20,47 @@ export default function Footer() {
 
                 {/* Social Icons */}
                 <motion.div 
-                    className="w-full text-center mb-20"
+                    className="w-full text-center mb-8"
                     initial={{ opacity: 0, scale: 0.8 }}
                     whileInView={{ opacity: 1, scale: 1 }}
                     transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
                     viewport={{ once: true }}
                 >
-                    <div className="inline-flex gap-5 justify-center">
+                    <div className="justify-center">
                         <SocialIcons />
                     </div>
                 </motion.div>
 
                 {/* Divider */}
-                <div className="w-full h-px bg-[#555555] mb-16"></div>
+                <div className="w-4/5 h-px bg-[#555555] lg:mb-10 sm: mb-4"></div>
 
                 {/* Footer Links Container */}
                 <motion.div 
-                    className="w-full grid sm:grid-cols-2 lg:grid-cols-none sm:flex-wrap lg:flex lg:flex-row gap-10 lg:gap-32 mb-16 lg:justify-start sm:justify-center"
+                    className="w-full grid sm:grid-cols-2 lg:grid-cols-none sm:flex-wrap lg:flex lg:flex-row gap-10 lg:gap-20 sm: mb-4 lg:mb-12 lg:justify-center sm:justify-center"
                     initial={{ opacity: 0, y: 50 }}
                     whileInView={{ opacity: 1, y: 0 }}
                     transition={{ duration: 0.8, ease: "easeInOut", delay: 0.3 }}
                     viewport={{ once: true }}
                 >
-                    <div className="flex flex-col items-center sm:col-span-2 lg:col-span-1 lg:items-start lg:order-first order-last">
-                        <span className="text-[#33C3A5] font-medium text-[16px] mb-4 font-['Asap_Condensed'] tracking-[0.01rem]">
+                    <div className="flex flex-col items-center sm:col-span-2 lg:col-span-1 lg:items-start">
+                        <span className="text-[#33C3A5] font-bold text-xl mb-4 tracking-[0.01rem]">
                             Our Apps
                         </span>
                         <img src="https://tv.eternityready.com/gp.png" alt="Play Store" 
-                            className="w-32 lg:w-40 rounded-md border cursor-pointer" />
+                            className="w-48 rounded-md border cursor-pointer" />
                         <img src="https://tv.eternityready.com/app-store.png" alt="App Store" 
-                            className="w-32 lg:w-40 rounded-md border cursor-pointer mt-3" />
+                            className="w-48 rounded-md border cursor-pointer mt-3" />
                     </div>
 
                     {/* Useful Links */}
-                   <div className="sm: grid sm: grid-cols-2 gap-10 lg:flex lg:gap-32"> 
+                   <div className="sm: grid sm: grid-cols-2 gap-10 lg:flex lg:gap-20"> 
                     <div className="flex flex-col items-start">
-                        <span className="text-[#33C3A5] font-medium text-[16px] mb-4 font-['Asap_Condensed'] tracking-[0.01rem]">
+                        <span className="text-[#33C3A5] font-bold text-xl mb-4 tracking-[0.01rem]">
                             Useful Links
                         </span>
                         <div className="flex flex-col gap-3">
                             {["Radio Schedule", "Ways to Listen", "Donate"].map((item) => (
-                                <span key={item} className="text-white font-normal font-['Asap_Condensed'] tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
+                                <span key={item} className="text-white font-normal tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
                                     {item}
                                 </span>
                             ))}
@@ -69,12 +69,12 @@ export default function Footer() {
 
                     {/* Our Brands */}
                     <div className="flex flex-col items-start">
-                        <span className="text-[#33C3A5] font-medium text-[16px] mb-4 font-['Asap_Condensed'] tracking-[0.01rem]">
+                        <span className="text-[#33C3A5] font-bold text-xl mb-4 tracking-[0.01rem]">
                             Our Brands
                         </span>
                         <div className="flex flex-col gap-3">
                             {["Corporate", "Eternity Ready TV", "Rapture Ready TV"].map((item) => (
-                                <span key={item} className="text-white font-normal font-['Asap_Condensed'] tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
+                                <span key={item} className="text-white font-normal tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
                                     {item}
                                 </span>
                             ))}
@@ -83,12 +83,12 @@ export default function Footer() {
 
                     {/* About */}
                     <div className="flex flex-col items-start">
-                        <span className="text-[#33C3A5] font-medium text-[16px] mb-4 font-['Asap_Condensed'] tracking-[0.01rem]">
+                        <span className="text-[#33C3A5] font-bold text-xl mb-4 tracking-[0.01rem]">
                             About
                         </span>
                         <div className="flex flex-col gap-3">
                             {["Team & Culture", "About Us", "Our Media Kit"].map((item) => (
-                                <span key={item} className="text-white font-normal font-['Asap_Condensed'] tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
+                                <span key={item} className="text-white font-normal tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
                                     {item}
                                 </span>
                             ))}
@@ -97,12 +97,12 @@ export default function Footer() {
 
                     {/* Help */}
                     <div className="flex flex-col items-start">
-                        <span className="text-[#33C3A5] font-medium text-[16px] mb-4 font-['Asap_Condensed'] tracking-[0.01rem]">
+                        <span className="text-[#33C3A5] font-bold text-xl mb-4 tracking-[0.01rem]">
                             Help
                         </span>
                         <div className="flex flex-col gap-3">
                             {["Terms of use", "Privacy & Legal", "Contact & Help"].map((item) => (
-                                <span key={item} className="text-white font-normal font-['Asap_Condensed'] tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
+                                <span key={item} className="text-white font-normal tracking-[0.01rem] hover:text-[#33C3A5] transition-colors cursor-pointer">
                                     {item}
                                 </span>
                             ))}
@@ -110,29 +110,16 @@ export default function Footer() {
                     </div>
                     </div>
                 </motion.div>
-
-                {/* Logo */}
+                <div className="mx-10 w-[calc(110%+2rem)] h-px bg-[#555555] mb-2"></div>
                 <motion.div 
-                    className="mb-5"
-                    initial={{ opacity: 0, y: 30 }}
-                    whileInView={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.8, ease: "easeInOut", delay: 0.4 }}
-                    viewport={{ once: true }}
-                >
-                    <span className="text-white font-bold text-[22px] flex items-center gap-1 tracking-[-2px]">
-                        ETERNITY <span className="text-[#E53935] font-bold tracking-[-2px]">READY</span>
-                    </span>
-                </motion.div>
-
-                {/* Copyright */}
-                <motion.div 
-                    className="mb-8"
+                    className="mb-4 flex lg:flex-row justify-between sm: flex-col lg:gap-52 sm: gap-2 items-center"
                     initial={{ opacity: 0 }}
                     whileInView={{ opacity: 1 }}
                     transition={{ duration: 0.8, delay: 0.6, ease: "easeInOut" }}
                     viewport={{ once: true }}
                 >
-                    <span className="text-[#777777] text-xs lg:text-[13px] font-['Asap_Condensed'] tracking-[0.01rem]">
+                    <img src="https://eternityready.com/templates/apollo/img/logo1USE-THIS.png" className="w-52"/>
+                    <span className="text-gray-200 sm: text-[13px] lg:text-[15px] tracking-[0.01rem] sm: text-center mt-1">
                         © 2012-2025 Eternity Ready LLC, All Rights Reserved.
                     </span>
                 </motion.div>
--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -3,13 +3,13 @@ import { FaXTwitter } from "react-icons/fa6"; // New X logo from react-icons v6
 
 const SocialIcons = () => {
   return (
-    <div className="flex space-x-8 flex-wrap justify-center gap-8">
+    <div className="flex space-x-8 flex-wrap justify-center gap-5">
       {/* Facebook */}
       <div className="flex items-center gap-3 hover:bg-white p-1 hover:px-10 rounded-4xl transition-all duration-900 cursor-pointer">
         <div className="w-4 h-4 flex items-center justify-center rounded-full"> 
           <FaFacebook className="text-xl" fill="#576EEE"/> 
         </div>
-        <span className="text-[#576EEE] font-bold" style={{ fontFamily: "Asap Condensed", letterSpacing: "0.01rem" }}>Facebook</span>
+        <span className="text-[#576EEE] font-bold">Facebook</span>
       </div>
 
       {/* Twitter X */}
@@ -17,7 +17,7 @@ const SocialIcons = () => {
         <div className="w-4 h-4 flex items-center justify-center rounded-full">
           <FaXTwitter className="text-[#1DBBFF] text-xl" />
         </div>
-        <span className="font-bold text-blue-400 font-oswald" style={{ fontFamily: "Asap Condensed", letterSpacing: "0.01rem" }}>x.com</span>
+        <span className="font-bold text-blue-400 font-oswald">Twitter</span>
       </div>
 
       {/* YouTube */}
@@ -25,7 +25,7 @@ const SocialIcons = () => {
         <div className="w-4 h-4 flex items-center justify-center rounded-full">
           <FaYoutube className="text-red-600 text-2xl" />
         </div>
-        <span className="text-red-600 font-bold font-asap" style={{ fontFamily: "Asap Condensed", letterSpacing: "0.01rem" }}>YouTube</span>
+        <span className="text-red-600 font-bold font-asap">Youtube</span>
       </div>
 
       {/* Instagram */}
@@ -33,7 +33,7 @@ const SocialIcons = () => {
         <div className="w-4 h-4 flex items-center justify-center">
           <FaInstagram className="text-2xl text-[#F8079a]" />
         </div>
-        <span className="text-[#F8079a] font-bold font-oswald" style={{ fontFamily: "Asap Condensed", letterSpacing: "0.01rem" }}>Instagram</span>
+        <span className="text-[#F8079a] font-bold font-oswald">Instagram</span>
       </div>
     </div>
   );

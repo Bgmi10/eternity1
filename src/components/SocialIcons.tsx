fe9390f8dfe8e29cdeeeeced1708cb60ce27ca88
subash <subashchandraboseravi45@gmail.com>
2025-02-17 07:18:46 +0530

    add

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -6,34 +6,34 @@ const SocialIcons = () => {
     <div className="flex space-x-8 flex-wrap justify-center gap-5">
       {/* Facebook */}
       <div className="flex items-center gap-3 hover:bg-white p-1 hover:px-10 rounded-4xl transition-all duration-900 cursor-pointer">
-        <div className="w-4 h-4 flex items-center justify-center rounded-full"> 
+        <div className="w-5 h-5 flex items-center justify-center rounded-full"> 
           <FaFacebook className="text-xl" fill="#576EEE"/> 
         </div>
-        <span className="text-[#576EEE] font-bold">Facebook</span>
+        <span className="text-[#576EEE] font-bold text-xl">Facebook</span>
       </div>
 
       {/* Twitter X */}
       <div className="flex gap-3 items-center hover:bg-white p-1 rounded-4xl transition-all duration-900 hover:px-10 cursor-pointer">
-        <div className="w-4 h-4 flex items-center justify-center rounded-full">
-          <FaXTwitter className="text-[#1DBBFF] text-xl" />
+        <div className="w-5 h-5 flex items-center justify-center rounded-full">
+          <FaXTwitter className="text-white text-xl" />
         </div>
-        <span className="font-bold text-blue-400 font-oswald">X.com</span>
+        <span className="font-bold text-blue-400 font-oswald text-xl">x.com</span>
       </div>
 
       {/* YouTube */}
       <div className="flex items-center gap-3 hover:bg-white p-1 rounded-4xl transition-all duration-900 hover:px-10 cursor-pointer">
-        <div className="w-4 h-4 flex items-center justify-center rounded-full">
+        <div className="w-5 h-5 flex items-center justify-center rounded-full">
           <FaYoutube className="text-red-600 text-2xl" />
         </div>
-        <span className="text-red-600 font-bold font-asap">Youtube</span>
+        <span className="text-red-600 font-bold font-asap text-xl">Youtube</span>
       </div>
 
       {/* Instagram */}
       <div className="flex items-center gap-3 hover:bg-white p-1 rounded-4xl transition-all duration-900 hover:px-10 cursor-pointer">  
-        <div className="w-4 h-4 flex items-center justify-center">
+        <div className="w-5 h-5 flex items-center justify-center">
           <FaInstagram className="text-2xl text-[#F8079a]" />
         </div>
-        <span className="text-[#F8079a] font-bold font-oswald">Instagram</span>
+        <span className="text-[#F8079a] font-bold font-oswald text-xl">Instagram</span>
       </div>
     </div>
   );

cbb07c344654ed86dbd0b3b23800588ec15e1839
subash <subashchandraboseravi45@gmail.com>
2025-02-19 22:31:36 +0530

    s

--- src/components/BottomNavBar.tsx
+++ src/components/BottomNavBar.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { FaFilm, FaTv, FaPodcast, FaMusic } from "react-icons/fa";
 import { FaRadio } from "react-icons/fa6";
 import { cn } from "../lib/utils";
